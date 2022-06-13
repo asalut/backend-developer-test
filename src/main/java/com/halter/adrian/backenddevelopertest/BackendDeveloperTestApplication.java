8f6888db7f12e50d5d7ca21053ec1a3fcaef49d7
ian <adriansalut@ymail.com>
2022-06-13 22:36:24 +1200

    base spring boot project with support for postgresql and redis

---
+++ src/main/java/com/halter/adrian/backenddevelopertest/BackendDeveloperTestApplication.java
@@ -0,0 +1,13 @@
+package com.halter.adrian.backenddevelopertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeveloperTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeveloperTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/halter/adrian/backenddevelopertest/BackendDeveloperTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halter.adrian.backenddevelopertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeveloperTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
